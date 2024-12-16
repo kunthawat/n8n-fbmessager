3097f90a99e8f4e3579ea1e1584b71929d0a0027
Kunthawat Greethong <kunthawat@moreminimore.com>
2024-12-16 13:11:00 +0700

    3 th new

--- old/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-exports.nodeTypes = void 0;
-var FacebookMessenger_node_1 = require("./FacebookMessenger/FacebookMessenger.node");
-var FacebookMessengerTrigger_node_1 = require("./FacebookMessenger/FacebookMessengerTrigger.node");
-exports.nodeTypes = {
-    facebookMessenger: new FacebookMessenger_node_1.FacebookMessenger(),
-    facebookMessengerTrigger: new FacebookMessengerTrigger_node_1.FacebookMessengerTrigger()
-};
--- old/index.ts
+++
@@ -1,8 +0,0 @@
-import { INodeTypes } from 'n8n-workflow';
-import { FacebookMessenger } from './FacebookMessenger/FacebookMessenger.node';
-import { FacebookMessengerTrigger } from './FacebookMessenger/FacebookMessengerTrigger.node';
-
-export const nodeTypes: INodeTypes = {
-    facebookMessenger: new FacebookMessenger(),
-    facebookMessengerTrigger: new FacebookMessengerTrigger(),
-};
