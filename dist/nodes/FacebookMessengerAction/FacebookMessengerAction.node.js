75d7620e4bbb830d4edfe2aea677accd0ea49e8e
Kunthawat Greethong <kunthawat@moreminimore.com>
2024-12-17 10:46:46 +0700

    6th new

--- dist/nodes/FacebookMessengerAction/FacebookMessengerAction.node.js
+++ dist/nodes/FacebookMessengerAction/FacebookMessengerAction.node.js
@@ -4,14 +4,14 @@ exports.FacebookMessengerAction = void 0;
 class FacebookMessengerAction {
     constructor() {
         this.description = {
-            displayName: 'Facebook Messenger',
-            name: 'facebookMessenger',
+            displayName: 'Facebook Messenger Action',
+            name: 'facebookMessengerAction',
             icon: 'file:facebook.svg',
             group: ['transform'],
             version: 1,
             description: 'Send messages through Facebook Messenger',
             defaults: {
-                name: 'Facebook Messenger',
+                name: 'Facebook Messenger Action',
             },
             inputs: [{ type: "main" }],
             outputs: [{ type: "main" }],
@@ -34,18 +34,6 @@ class FacebookMessengerAction {
                             description: 'Send a message',
                             action: 'Send a message',
                         },
-                        {
-                            name: 'Send Template',
-                            value: 'sendTemplate',
-                            description: 'Send a template message',
-                            action: 'Send a template message',
-                        },
-                        {
-                            name: 'Send Media',
-                            value: 'sendMedia',
-                            description: 'Send media (image, video, file)',
-                            action: 'Send media',
-                        },
                     ],
                     default: 'sendMessage',
                 },
@@ -55,66 +43,109 @@ class FacebookMessengerAction {
                     type: 'string',
                     default: '',
                     required: true,
-                    description: 'The ID of the message recipient',
+                    description: 'The PSID of the message recipient',
+                    displayOptions: {
+                        show: {
+                            operation: ['sendMessage'],
+                        },
+                    },
                 },
                 {
                     displayName: 'Message Text',
                     name: 'messageText',
                     type: 'string',
                     default: '',
+                    required: true,
+                    description: 'The text to send',
                     displayOptions: {
                         show: {
                             operation: ['sendMessage'],
                         },
                     },
-                    required: true,
-                    description: 'The text to send',
                 },
             ],
         };
     }
     async execute() {
+        var _a, _b, _c, _d;
         const items = this.getInputData();
         const returnData = [];
         const credentials = await this.getCredentials('facebookApi');
-        const operation = this.getNodeParameter('operation', 0);
+        if (!(credentials === null || credentials === void 0 ? void 0 : credentials.accessToken)) {
+            throw new Error('Access Token is required!');
+        }
         try {
             for (let i = 0; i < items.length; i++) {
-                const recipientId = this.getNodeParameter('recipientId', i);
-                const messageData = {
-                    messaging_type: 'RESPONSE',
-                    recipient: {
-                        id: recipientId,
-                    },
-                    message: {},
-                };
+                const operation = this.getNodeParameter('operation', i);
                 if (operation === 'sendMessage') {
+                    const recipientId = this.getNodeParameter('recipientId', i);
                     const messageText = this.getNodeParameter('messageText', i);
-                    messageData.message = {
-                        text: messageText,
+                    if (!recipientId) {
+                        throw new Error('Recipient ID is required!');
+                    }
+                    if (!messageText) {
+                        throw new Error('Message text is required!');
+                    }
+                    const requestBody = {
+                        recipient: {
+                            id: recipientId,
+                        },
+                        message: {
+                            text: messageText,
+                        },
+                        messaging_type: 'RESPONSE',
                     };
+                    console.log('Facebook Messenger Request:', {
+                        url: 'https://graph.facebook.com/v17.0/me/messages',
+                        method: 'POST',
+                        recipientId,
+                        messageLength: messageText.length,
+                        accessToken: `${String(credentials.accessToken).substring(0, 5)}...`,
+                    });
+                    try {
+                        const response = await this.helpers.request({
+                            method: 'POST',
+                            url: 'https://graph.facebook.com/v17.0/me/messages',
+                            headers: {
+                                'Content-Type': 'application/json',
+                            },
+                            qs: {
+                                access_token: credentials.accessToken,
+                            },
+                            body: requestBody,
+                            resolveWithFullResponse: true,
+                        });
+                        returnData.push({
+                            json: {
+                                success: true,
+                                ...response,
+                            },
+                        });
+                    }
+                    catch (error) {
+                        console.error('Facebook API Error:', {
+                            status: error.statusCode,
+                            message: error.message,
+                            response: (_a = error.response) === null || _a === void 0 ? void 0 : _a.body,
+                        });
+                        if ((_b = error.response) === null || _b === void 0 ? void 0 : _b.body) {
+                            const errorBody = typeof error.response.body === 'string'
+                                ? JSON.parse(error.response.body)
+                                : error.response.body;
+                            throw new Error(`Facebook API Error (${error.statusCode}): ${((_c = errorBody.error) === null || _c === void 0 ? void 0 : _c.message) || error.message}`);
+                        }
+                        throw error;
+                    }
                 }
-                const response = await this.helpers.httpRequest({
-                    method: 'POST',
-                    url: 'https://graph.facebook.com/v13.0/me/messages',
-                    headers: {
-                        'Content-Type': 'application/json',
-                    },
-                    qs: {
-                        access_token: credentials.accessToken,
-                    },
-                    body: messageData,
-                });
-                returnData.push({
-                    json: response,
-                });
             }
         }
         catch (error) {
             if (this.continueOnFail()) {
                 returnData.push({
                     json: {
+                        success: false,
                         error: error.message,
+                        details: ((_d = error.response) === null || _d === void 0 ? void 0 : _d.body) || {},
                     },
                 });
             }
